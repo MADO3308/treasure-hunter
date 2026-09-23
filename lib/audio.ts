b65e075b1c22a1b3506ea755930c59e10f486776
Mazen Khalil <mazenahmed1721@gmail.com>
2026-09-23 07:21:14 +0300

    created autdio.ts

---
+++ lib/audio.ts
@@ -0,0 +1,68 @@
+class SoundEngine {
+    private ctx: AudioContext | null = null;
+    private init() {
+        if (!this.ctx && typeof window !== 'undefined') {
+            const AudioCtx = window.AudioContext || (window as unknown as { webkitAudioContext: typeof AudioContext }).webkitAudioContext;
+            this.ctx = new AudioCtx();
+        }
+    }
+    platTypewriter() {
+        this.init();
+        if (!this.ctx) return;
+        const osc = this.ctx.createOscillator();
+        const gain = this.ctx.createGain();
+        osc.type = 'triangle';
+        osc.frequency.setValueAtTime(400 + Math.random() * 200, this.ctx.currentTime);
+        gain.gain.setValueAtTime(0.015, this.ctx.currentTime);
+        gain.gain.exponentialRampToValueAtTime(0.001, this.ctx.currentTime + 0.04);
+        osc.connect(gain);
+        gain.connect(this.ctx.destination);
+        osc.start();
+        osc.stop(this.ctx.currentTime + 0.04);
+    }
+    playClick() {
+        this.init();
+        if (!this.ctx) return;
+        const osc = this.ctx.createOscillator();
+        const gain = this.ctx.createGain();
+        osc.type = 'sine';
+        osc.frequency.setValueAtTime(800, this.ctx.currentTime);
+        osc.frequency.exponentialRampToValueAtTime(200, this.ctx.currentTime + 0.05);
+        gain.gain.setValueAtTime(0.05, this.ctx.currentTime);
+        gain.gain.linearRampToValueAtTime(0, this.ctx.currentTime + 0.05);
+        osc.connect(gain);
+        gain.connect(this.ctx.destination);
+        osc.start();
+        osc.stop(this.ctx.currentTime + 0.05);
+    }
+    playSuccess() {
+        this.init();
+        if (!this.ctx) return;
+        [523.25, 659.25, 783.99, 1046.50].forEach((freq, idx) => {
+            const osc = this.ctx!.createOscillator();
+            const gain = this.ctx!.createGain();
+            osc.type = 'sine';
+            osc.frequency.setValueAtTime(freq, this.ctx!.currentTime + idx * 0.08);
+            gain.gain.setValueAtTime(0.06, this.ctx!.currentTime + idx * 0.08);
+            gain.gain.exponentialRampToValueAtTime(0.001, this.ctx!.currentTime + idx * 0.08 + 0.3);
+            osc.connect(gain);
+            gain.connect(this.ctx!.destination);
+            osc.start(this.ctx!.currentTime + idx * 0.08);
+            osc.stop(this.ctx!.currentTime + idx * 0.08 + 0.3);
+        });
+    }
+    playVaultClick() {
+        this.init();
+        if (!this.ctx) return;
+        const osc = this.ctx.createOscillator();
+        const gain = this.ctx.createGain();
+        osc.type = 'square';
+        osc.frequency.setValueAtTime(120, this.ctx.currentTime);
+        gain.gain.setValueAtTime(0.08, this.ctx.currentTime + 0.03);
+        osc.connect(gain);
+        gain.connect(gain);
+        osc.start();
+        osc.stop(this.ctx.currentTime + 0.03);
+    }
+}
+export const soundFx = new SoundEngine();
